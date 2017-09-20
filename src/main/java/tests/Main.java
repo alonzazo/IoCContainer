062ba8da7347862165df02579a9702f55a5d09d3
hernan28 <hernanmadrigal28@gmail.com>
2017-09-20 13:06:31 -0600

    hi sirve

--- src/main/java/tests/Main.java
+++ src/main/java/tests/Main.java
@@ -1,7 +1,9 @@
 package tests;
 
+import parsers.AnnotationParser;
+
 public class Main {
     public static void main(String[] args) {
-
+        AnnotationParser parser = new AnnotationParser();
     }
 }
