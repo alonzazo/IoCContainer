e6cde07f2a2cc0dc6b494292d5b849353e2f9e5f
Roberto <Roberto@DESKTOP-07K9S1N>
2017-09-25 03:27:54 -0600

    Added attributes ref and value to hold XML info Removed attribute type Implemented toString()

--- src/main/java/containers/Property.java
+++ src/main/java/containers/Property.java
@@ -2,12 +2,45 @@ package containers;
 
 public class Property {
 
-    String name;
-    Object value;
-    Class type;
+    private String name;
+    private Object instance;
+    private String ref;
+    private String value;
 
-    public Property()
-    {
 
+    public Property() {}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public String toString() {
+        String str = "  Property name: "+name;
+        if(value != null) {
+            str += "\n  value: "+value;
+        } else if (ref != null) {
+            str += "\n  ref: "+ref;
+        }
+        return str;
     }
 }
