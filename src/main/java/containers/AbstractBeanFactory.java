4bc55aad0ac42a39e4936ccf61ac1a4afa6ef9e9
Roberto <Roberto@DESKTOP-07K9S1N>
2017-09-25 03:24:29 -0600

    Added exception handling to getBeans() Implemented printBean() and printBeans() Added Parser attribute

--- src/main/java/containers/AbstractBeanFactory.java
+++ src/main/java/containers/AbstractBeanFactory.java
@@ -1,19 +1,39 @@
 package containers;
 
+import parsers.BeanConfigurationException;
+import parsers.Parser;
+
 import java.util.HashMap;
 
 public abstract class AbstractBeanFactory implements BeanFactory {
 
+    protected Parser parser;
     protected HashMap<String, Bean> beans;
 
-    public AbstractBeanFactory()
-    {
+    public AbstractBeanFactory() {
         beans = new HashMap<>();
     }
 
-    public void addBean(String name, Bean bean) {
-        beans.put(name, bean);
+    public void addBean(String key, Bean bean) throws BeanConfigurationException{
+        if (beans.containsKey(key)) {
+            throw new BeanConfigurationException("Multiple definitions for bean: "+key);
+        } else {
+            beans.put(key, bean);
+        }
     }
 
+    public String printBean(String key) {
+        return beans.get(key).toString();
+    }
 
+    public String printBeans() {
+        String str = "";
+        for(Object bean : beans.values()) {
+            str += bean.toString();
+        }
+        return str;
+    }
+
+    //TODO bean injection
+    public Object getBean(String name){return new Object();}
 }
