3908cf143888e6e018fedd524b8b0f04b4fcf462
Roberto <Roberto@DESKTOP-07K9S1N>
2017-09-25 03:26:35 -0600

    Changed scan declaration to throw BeanConfigurationException Added methods:  -addBean()  -getBean  -printBean() and printBeans()

--- src/main/java/containers/BeanFactory.java
+++ src/main/java/containers/BeanFactory.java
@@ -1,5 +1,10 @@
 package containers;
+import parsers.BeanConfigurationException;
 
 public interface BeanFactory {
-    void scan(String s);
+    void scan(String s) throws BeanConfigurationException;
+    void addBean(String name, Bean bean) throws BeanConfigurationException;
+    Object getBean(String name);
+    String printBean(String key);
+    String printBeans();
 }
